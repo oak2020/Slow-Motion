ba9aba169a7678c8cba1b55c4a77db4d7df5bd94
mohamad <mohamad13962018@gmail.com>
2020-04-09 00:17:22 +0430

    Upload

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1586375063,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/green_right-sheet0.png",
+		"images/green_right-sheet1.png",
+		"images/solid.png",
+		"images/copyrite-sheet0.png",
+		"images/text-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
